1c3f8e16bf7359bd546d9139c614c124302afe2e
YetiBarBar <yetibarbar@gmail.com>
2023-12-01 21:49:55 +0100

    Prepare: Day 2

---
+++ src/day2.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let data = include_str!("../data/day_2023_2.data");
+
+    println!("Part 1: {}", part1(&data));
+    println!("Part 2: {}", part2(&data));
+}
+
+fn part1(_data: &[&str]) -> usize {
+    todo!()
+}
+
+fn part2(_data: &[&str]) -> usize {
+    todo!()
+}
